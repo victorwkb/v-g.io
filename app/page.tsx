9684bf2a09ad81f9f3796331030822f1ee546cdb
victorwkb <victorwkb@gmail.com>
2023-11-12 19:52:04 +1100

    fix contentlayer config

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default async function Home() {
+  return (
+    <div>
+      <h1>Victor Goh home</h1>
+    </div>
+  )
+}
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,40 +4,29 @@ import rehypeSlug from 'rehype-slug';
 
 const getSlug = (doc: any) => doc._raw.sourceFileName.replace(/\.mdx$/, '');
 
-const projectComputedFields = {
+const projectComputedFields: ComputedFields = {
   slug: {
     type: 'string',
     resolve: (doc) => getSlug(doc),
   },
   image: {
     type: 'string',
-    resolve: (doc) => `/projects${getSlug(doc)}/image.png`,
-  }
-}
+    resolve: (doc) => `/projects/${getSlug(doc)}/image.png`,
+  },
+};
 
 export const Project = defineDocumentType(() => ({
   name: 'Project',
-  filePathPattern: `**/*.mdx`,
+  filePathPattern: `projects/**/*.mdx`,
   contentType: 'mdx',
   fields: {
-    title: {
-      type: 'string',
-      required: true,
-    },
-    summary: {
-      type: 'string',
-      required: true,
-    },
-    publishedAt: {
-      type: 'string',
-      required: false,
-    },
-    updatedAt: {
-      type: 'string',
-      required: false,
-    },
-    computedFields: projectComputedFields,
-  }
+    title: { type: 'string', required: true },
+    date: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    url: { type: 'string', required: false },
+    tags: { type: 'json', required: false },
+  },
+  computedFields: projectComputedFields,
 }));
 
 export default makeSource({
@@ -45,5 +34,5 @@ export default makeSource({
   documentTypes: [Project],
   mdx: {
     rehypePlugins: [rehypePrism, rehypeSlug],
-  }
-})
+  },
+});
