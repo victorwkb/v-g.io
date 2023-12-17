48f14d87188aec50f187a92defd777114eec6fb2
victorwkb <victorwkb@gmail.com>
2023-12-17 20:04:31 +1100

    about page template

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Metadata } from "next";
+import Section from "app/components/ui/Section";
 
 export const metadata: Metadata = {
   title: "About",
@@ -7,5 +8,60 @@ export const metadata: Metadata = {
 };
 
 export default function About() {
-  return <div>about page placeholder</div>;
+  return (
+    <div className="flex flex-col gap-16">
+      <div>
+        <h1 className="animate-in text-3xl font-bold tracking-tight">
+          About Me
+        </h1>
+        <p
+          className="animate-in text-secondary"
+          style={{ "--index": 1 } as React.CSSProperties}
+        >
+          A glimpse of myself.
+        </p>
+      </div>
+
+      <div className="columns-2">
+        <div className="relative mb-4 h-40">
+          <Image
+            alt="Placeholder"
+            src="/next.svg"
+            layout="fill"
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-80">
+          <Image
+            alt="Placeholder"
+            src="/next.svg"
+            layout="fill"
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative mb-4 h-80">
+          <Image
+            alt="Placeholder"
+            src="/next.svg"
+            layout="fill"
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-40">
+          <Image
+            alt="Placeholder"
+            src="/next.svg"
+            layout="fill"
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+
+      <Section heading="About" headingAlignment="left">
+        <div className="flex flex-col gap-6">
+          <p></p>
+        </div>
+      </Section>
+    </div>
+  );
 }
---
+++ app/components/ui/Section.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import clsx from "clsx";
+import { useTheme } from "next-themes";
+import { ReactNode } from "react";
+
+type SectionProps = {
+  heading: string;
+  headingAlignment?: "right" | "left";
+  children: ReactNode;
+};
+
+export default function Section({
+  heading,
+  headingAlignment,
+  children,
+}: SectionProps) {
+  const { theme } = useTheme();
+
+  return (
+    <section className="flex flex-col md:flex-row gap-2 md:gap-9 col-reverse">
+      <h2
+        className={clsx(
+          "md:w-32 text-secondary shrink-0",
+          headingAlignment === "right" && "md:text-right",
+          theme === "terminal" ? "font-mono tracking-tight" : ""
+        )}
+      >
+        {heading}
+      </h2>
+      {children}
+    </section>
+  );
+}
