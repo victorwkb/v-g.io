6f8a96fce6f7ae0f387de86b970e036971618954
victorwkb <victorwkb@gmail.com>
2024-03-18 21:50:52 +1100

    fix contentlayer

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,18 +1,12 @@
-import { allProjects, Post as PostType } from ".contentlayer/generated";
+import { allProjects } from ".contentlayer/generated";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 
-type PostProps = {
-  post: PostType;
-  related: PostType[];
-};
-
 export default function Project({ params }: { params: any }) {
-  // const post = allPosts.find((post) => post.slug === params.slug);
-  const post = allProjects.find((post) => post.slug === params.slug);
+  const proj = allProjects.find((proj) => proj.slug === params.slug);
 
-  if (!post) {
+  if (!proj) {
     notFound();
   }
 
@@ -21,24 +15,24 @@ export default function Project({ params }: { params: any }) {
       <article>
         <div className="flex animate-in flex-col gap-3">
           <div className="flex gap-3 text-secondary">
-            <p>{post.date}</p>
-            {post.url && (
+            <p>{proj.date}</p>
+            {proj.url && (
               <>
                 <span>&middot;</span>
-                <Link href={post.url} className="hover:text-primary">
+                <Link href={proj.url} className="hover:text-primary">
                   Visit Project
                 </Link>
               </>
             )}
           </div>
           <h1 className="text-3xl font-bold leading-tight tracking-tight text-primary">
-            {post.title}
+            {proj.title}
           </h1>
           <p
             className="animate-in text-lg leading-tight text-secondary md:text-xl"
             style={{ "--index": 1 } as React.CSSProperties}
           >
-            {post.description}
+            {proj.description}
           </p>
         </div>
 
@@ -48,7 +42,7 @@ export default function Project({ params }: { params: any }) {
         <div className="flex flex-col gap-6">
           <h2>Tags</h2>
           <div className="flex flex-wrap gap-3 ">
-            {post.tags.map((tag: string) => (
+            {proj.tags.map((tag: string) => (
               <div
                 key={tag}
                 className="whitespace-nowrap rounded-lg bg-secondary px-4 py-1.5 text-sm text-secondary"
